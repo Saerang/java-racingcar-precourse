0ed7d04c7cbc524f22f4cbb912285f1f9de07b1a
saerang <lsg10119@gmail.com>
2022-04-20 23:47:08 +0900

    feat(racingCars): 자동차 경주 시작 및 결과 확인

--- src/test/java/racingcar/RacingCarsTest.java
+++ src/test/java/racingcar/RacingCarsTest.java
@@ -2,7 +2,10 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 class RacingCarsTest {
 
@@ -15,5 +18,26 @@ class RacingCarsTest {
                                               .containsExactly("sae", "rang", "pro", "free", "pass");
     }
 
+    @Test
+    void 자동차_경주_2명_시작() {
+        RacingCars racingCars = new RacingCars("sae,rang");
+        racingCars.start(3, new TestRandomNumberGenerator(Arrays.asList(4, 1, 4, 2, 3, 5)));
+
+        assertThat(racingCars.getRacingCars()).hasSize(2)
+                                              .extracting("name", "location")
+                                              .containsExactly(tuple("sae", "--"), tuple("rang", "-"));
+    }
+
+    @Test
+    void 자동차_경주_3명_시작() {
+        RacingCars racingCars = new RacingCars("sae,rang,free");
+        racingCars.start(2, new TestRandomNumberGenerator(Arrays.asList(4, 1, 4, 2, 3, 5)));
+
+        assertThat(racingCars.getRacingCars()).hasSize(3)
+                                              .extracting("name", "location")
+                                              .containsExactly(tuple("sae", "-"),
+                                                               tuple("rang", ""),
+                                                               tuple("free", "--"));
+    }
 
 }
